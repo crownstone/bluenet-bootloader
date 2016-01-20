98f0f0206aa83322e95776e4f9e51a22c277e6fb
Vliedel <bart@dobots.nl>
2016-01-20 11:51:08 +0100

    Include the .default and .local config files

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,8 +1,28 @@
 #!/bin/sh
 
-if [ -z $BLUENET_CONFIG_DIR ]; then
+# TODO: don't use BLUENET_DIR ?
+if [ ! -d "${BLUENET_DIR}" ]; then
+	echo "ERROR: environment variable 'BLUENET_DIR' should be set."
+	exit 1
+fi
+
+if [ -e "${BLUENET_DIR}/CMakeBuild.config.default" ]; then
+        echo "source ${BLUENET_DIR}/CMakeBuild.config.default"
+        source "${BLUENET_DIR}/CMakeBuild.config.default"
+fi
+
+if [ -e "${BLUENET_DIR}/CMakeBuild.config.local" ]; then
+        echo "source ${BLUENET_DIR}/CMakeBuild.config.local"
+        source "${BLUENET_DIR}/CMakeBuild.config.local"
+fi
+
+
+if [ ! -d "$BLUENET_CONFIG_DIR" ]; then
 	echo "ERROR: environment variable 'BLUENET_CONFIG_DIR' should be set."
 	exit 1
 fi
 
-source ${BLUENET_CONFIG_DIR}/CMakeBuild.config
+echo "COMPILER_PATH=${COMPILER_PATH}"
+
+echo "source ${BLUENET_CONFIG_DIR}/CMakeBuild.config"
+source "${BLUENET_CONFIG_DIR}/CMakeBuild.config"
