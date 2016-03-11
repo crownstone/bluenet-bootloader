021f62ff98f3b26284f587dc9346fa1d8b31b6a0
Dominik Egger <dominik@almende.org>
2016-03-11 13:02:19 +0100

    reorganized. removed everything unneccessary

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -15,7 +15,7 @@ device_variant=xxaa
 output_path=${BLUENET_CONFIG_DIR}/build
 mkdir -p "$output_path"
 
-echo "++ Go to \"${path}/../nrf51822_v6.0.0 - GCC_Bootloader/Board/nrf6310/device_firmware_updates/bootloader - gcc - BLE/gcc\""
-cd "${path}/../nrf51822_v6.0.0 - GCC_Bootloader/Board/nrf6310/device_firmware_updates/bootloader - gcc - BLE/gcc"
+echo "++ Go to \"${path}/../src/gcc\""
+cd "${path}/../src/gcc"
 
 make clean
