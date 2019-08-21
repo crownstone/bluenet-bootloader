0245697ab5d1186a623c324e0b648a17cd132d38
Naveen <naveen.j_94@outlook.com>
2019-08-21 12:43:30 +0200

    Add provision for package name

--- intermediate/create.sh
+++ intermediate/create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Usage: $ ./create.sh [version]
+# Usage: $ ./create.sh [package_name version]
 # version = 1.8.0 or 1.9.0
 
 VER_1_8=1.8.0
@@ -9,17 +9,17 @@ VER_1_9=1.9.0
 prepare() {
     echo Preparing package for $1
     make clean
-    make VER=$1 package
+    make VER=$1 package PACKAGE_PREFIX=$2
 }
 
-if [ "$1" == $VER_1_8 ]; then
+if [ "$2" == $VER_1_8 ]; then
     echo Commanded v$VER_1_8
-    prepare $VER_1_8
-elif [ "$1" == $VER_1_9 ]; then
+    prepare $VER_1_8 $1
+elif [ "$2" == $VER_1_9 ]; then
     echo Commanded v$VER_1_9
-    prepare $VER_1_9
+    prepare $VER_1_9 $1
 else
     echo Commanded both v$VER_1_8 and v$VER_1_9
-    prepare $VER_1_8
-    prepare $VER_1_9
+    prepare $VER_1_8 $1
+    prepare $VER_1_9 $1
 fi
