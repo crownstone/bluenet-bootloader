d868454874f8b2829c2fb6f92c8bba2fa1a3a1fb
vliedel <bart@crownstone.rocks>
2019-10-15 16:48:20 +0200

    Create a package per different default hardware board.

--- intermediate/create.sh
+++ intermediate/create.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-# Usage: $ ./create.sh [package_name version]
-# version = 1.8.0 or 1.9.0
-
-VER_1_8=1.8.0
-VER_1_9=1.9.0
-
-prepare() {
-    echo Preparing package for $1
-    make clean
-    make VER=$1 package PACKAGE_PREFIX=$2
+usage() {
+	echo "Usage: $0 <package_prefix>"
 }
 
-if [ "$2" == $VER_1_8 ]; then
-    echo Commanded v$VER_1_8
-    prepare $VER_1_8 $1
-elif [ "$2" == $VER_1_9 ]; then
-    echo Commanded v$VER_1_9
-    prepare $VER_1_9 $1
-else
-    echo Commanded both v$VER_1_8 and v$VER_1_9
-    prepare $VER_1_8 $1
-    prepare $VER_1_9 $1
+prepare() {
+	echo "Preparing package for $1"
+	make clean
+	make VER=$1 DEFAULT_HARDWARE_BOARD=$3 package PACKAGE_PREFIX=$2 
+}
+
+if [ $# -ne 1 ]; then
+	usage
+	exit 1
 fi
+
+echo "Create both v1.8 and v1.9 for all default hardware boards."
+prepare "1.8.0" $1 "ACR01B2C"
+prepare "1.9.0" $1 "ACR01B2C"
+prepare "1.8.1" $1 "GUIDESTONE"
+prepare "1.9.1" $1 "GUIDESTONE"
